702b7ebda4a45c2882519b673834274c62ced5c1
Liuww <wenwei.liu@leadchina.cn>
2021-07-16 21:19:56 +0800

    控件功能扩展演示程序

---
+++ ControlFunctionExtensionSample/WaterMarkHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace ControlFunctionExtensionSample
+{
+    public class WaterMarkHelper
+    {
+        public static string GetWaterMark(DependencyObject obj)
+        {
+            return (string)obj.GetValue(WaterMarkProperty);
+        }
+
+        public static void SetWaterMark(DependencyObject obj, string value)
+        {
+            obj.SetValue(WaterMarkProperty, value);
+        }
+
+        public static readonly DependencyProperty WaterMarkProperty =
+            DependencyProperty.RegisterAttached("WaterMark", typeof(string), typeof(WaterMarkHelper), new PropertyMetadata(null));
+
+
+    }
+}
---
+++ ControlFunctionExtensionSample/WaterMarkTextBox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlFunctionExtensionSample
+{
+    public class WaterMarkTextBox : TextBox
+    {
+        static WaterMarkTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WaterMarkTextBox), new FrameworkPropertyMetadata(typeof(WaterMarkTextBox)));
+        }
+
+
+        public string WaterMark
+        {
+            get { return (string)GetValue(WaterMarkProperty); }
+            set { SetValue(WaterMarkProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for WaterMark.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WaterMarkProperty =
+            DependencyProperty.Register("WaterMark", typeof(string), typeof(WaterMarkTextBox), new PropertyMetadata(null));
+
+
+    }
+}
