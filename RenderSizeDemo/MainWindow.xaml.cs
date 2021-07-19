dacec523a3927ed672cd6f722c99bcfd6b1d2f7c
Liuww <wenwei.liu@leadchina.cn>
2021-07-19 16:15:59 +0800

    RenderSize demo

---
+++ RenderSizeDemo/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RenderSizeDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private double _scale = 1.0d;
+        private void ScaleButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _scale += 0.1;
+            canvas.RenderTransform = new ScaleTransform(_scale,_scale);
+        }
+    }
+}
