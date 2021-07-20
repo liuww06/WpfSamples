a927f6d18f683e17dabb41401cfb4ebefe46c0cd
Liuww <wenwei.liu@leadchina.cn>
2021-07-20 10:34:20 +0800

    RenderTransform  use  MatrixTransform

--- RenderSizeDemo/MainWindow.xaml.cs
+++ RenderSizeDemo/MainWindow.xaml.cs
@@ -23,13 +23,27 @@ namespace RenderSizeDemo
         public MainWindow()
         {
             InitializeComponent();
+            canvas.MouseMove += CanvasOnMouseMove;
+        }
+
+        private void CanvasOnMouseMove(object sender, MouseEventArgs e)
+        {
+            var p = e.GetPosition(canvas);
+            tbPosition.Text = $"X:{p.X},Y:{p.Y}";
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            canvas.RenderTransform = new MatrixTransform(2,0,0,-2,0,200);
+            rect.RenderTransform = new MatrixTransform(1, 0, 0, -1, -231, 78);
+            base.OnInitialized(e);
         }
 
         private double _scale = 1.0d;
         private void ScaleButton_OnClick(object sender, RoutedEventArgs e)
         {
             _scale += 0.1;
-            canvas.RenderTransform = new ScaleTransform(_scale,_scale);
+            rect.RenderTransform = new ScaleTransform(_scale,_scale);
         }
     }
 }
